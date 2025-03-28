6793f8c3c718ee429727e2353172ec11d1f9e443
yeabTesfaye <tesfayeyeabsra446@gmail.com>
2025-03-28 14:14:53 +0300

    feat : add a header section

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,8 @@
+export const THEMES = {
+  LIGHT: 'light',
+  DARK: 'dark',
+} as const;
+
 export const links = [
   {
     name: 'Home',
@@ -25,9 +30,78 @@ export const links = [
   },
 ] as const;
 
+/*
+export const experiencesData = [
+  {
+    title: 'Computer Science Graduate',
+    location: 'Addis Ababa, Ethiopia',
+    description:
+      'Graduated with a degree in Computer Science with distinction, gaining a strong foundation in algorithms, data structures, and software development.',
+    icon: React.createElement(LuGraduationCap),
+    date: '2023',
+  },
+  {
+    title: 'Software Developer',
+    location: 'Remote, Freelance',
+    description:
+      'Specialized in frontend development, leveraging Next.js to build responsive, high-performance, and visually appealing user interfaces.',
+    icon: React.createElement(CgWorkAlt),
+    date: '2023 - 2025',
+  },
+  {
+    title: 'Penetration Tester',
+    location: 'Addis Ababa, Ethiopia',
+    description:
+      'Currently working as a full-stack developer and freelance penetration tester. Skilled in React, Next.js, TypeScript, Tailwind, Prisma, and MongoDB. Open to full-time remote opportunities.',
+    icon: React.createElement(FaReact),
+    date: '2024 - Present',
+  },
+] as const;
 
 
-export const THEMES = {
-  LIGHT: 'light',
-  DARK: 'dark',
-} as const;
+export const projectsData = [
+  {
+    title: 'Prostore',
+    description:
+      'A fully-fledged full-stack e-commerce platform with PayPal integration.',
+    tags: ['React', 'Next.js', 'Tailwind', 'Prisma', 'TypeScript'],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: 'E-Learning Platform',
+    description: 'A comprehensive e-learning platform built using .NET.',
+    tags: ['.NET', 'C#', 'Docker', 'SQL Server'],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: 'Ticket Tracking System',
+    description:
+      'A demo project for a Ticket Management System, designed to handle users, tickets, and workflows efficiently.',
+    tags: ['React', 'MongoDB', 'Bootstrap', 'Node.js'],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'Tailwind',
+  'Prisma',
+  'MongoDB',
+  'Redux',
+  'GraphQL',
+  'Apollo',
+  'Express',
+  'PostgreSQL',
+  'Python',
+  'Django',
+  'Framer Motion',
+] as const;
+
+*/
